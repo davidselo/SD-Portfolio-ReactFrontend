b03ef228b4295d0796c8f56f2b57c35171147c41
David Villalba Flores <david.selo@gmail.com>
2022-09-30 15:55:02 +0100

    Adding more Header Component tests

--- src/components/Header/__test__/Header.spec.tsx
+++ src/components/Header/__test__/Header.spec.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {render, screen, fireEvent} from '@testing-library/react';
 import Header from '../index';
 import MenuIcon from '@mui/icons-material/Menu';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 describe('Header component', () => {
     it('Should match test snapshot', () => {
@@ -19,6 +20,20 @@ describe('MenuIcon component', () => {
             />,
         );
         const element = screen.getByTestId('header--burger-menu-icon');
+        fireEvent.click(element);
+        expect(handleClick).toHaveBeenCalledTimes(1);
+    });
+});
+describe('ChevronRightIcon component', () => {
+    it('calls onClick prop when clicked', () => {
+        const handleClick = jest.fn();
+        render(
+            <ChevronRightIcon
+                onClick={handleClick}
+                data-testid="header--chevron-icon"
+            />,
+        );
+        const element = screen.getByTestId('header--chevron-icon');
         fireEvent.click(element);
         expect(handleClick).toHaveBeenCalledTimes(1);
     });
