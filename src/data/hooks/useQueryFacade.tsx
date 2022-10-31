5d6881fbbbc4cd587541725631b0b4f90568954c
David Villalba Flores <david.selo@gmail.com>
2022-10-31 17:57:13 +0100

    Spelling issue fixed

--- src/data/hooks/useQueryFacade.tsx
+++ src/data/hooks/useQueryFacade.tsx
@@ -2,17 +2,17 @@ import {DocumentNode, useQuery} from '@apollo/client';
 import {Skills} from 'contracts/Skills';
 import {getStaticData} from 'data/staticData/DataMapping';
 
-const STATIC_PERSISTANCE_MODE = 'static';
-const GRAPHQL_PERSISTANCE_MODE = 'headless';
+const STATIC_PERSISTENCE_MODE = 'static';
+const GRAPHQL_PERSISTENCE_MODE = 'headless';
 
 const useQueryFacade = (
     collectionName: string, // collection name for static mode.
     grapQLQuery: DocumentNode, // GraphQL query for Headless mode.
 ) => {
     const {loading, error, data} = useQuery<Skills>(grapQLQuery);
-    if (process.env.PERSISTANCE_MODE == STATIC_PERSISTANCE_MODE) {
+    if (process.env.PERSISTENCE_MODE == STATIC_PERSISTENCE_MODE) {
         return getStaticData(collectionName);
-    } else if (process.env.PERSISTANCE_MODE == GRAPHQL_PERSISTANCE_MODE) {
+    } else if (process.env.PERSISTENCE_MODE == GRAPHQL_PERSISTENCE_MODE) {
         return {loading, error, data};
     }
 };
