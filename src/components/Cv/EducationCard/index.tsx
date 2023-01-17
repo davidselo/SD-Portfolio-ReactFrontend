437b692d72bd83e5f096929024daf782450e18d1
David Villalba Flores <david.selo@gmail.com>
2023-01-17 19:57:10 +0000

    Adding Tests to EducationCard

--- src/components/Cv/EducationCard/index.tsx
+++ src/components/Cv/EducationCard/index.tsx
@@ -1,13 +1,5 @@
 import React from 'react';
-import {
-    Box,
-    Container,
-    Grid,
-    List,
-    ListItem,
-    ListItemText,
-    Typography,
-} from '@mui/material';
+import {Box, Container, Typography} from '@mui/material';
 import SchoolIcon from '@mui/icons-material/School';
 
 interface Props {
