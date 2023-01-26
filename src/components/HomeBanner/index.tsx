2e16063a085aee0e2c09f86ab423000942ea09d0
David Villalba Flores <david.selo@gmail.com>
2023-01-26 17:22:05 +0000

    Removing animation which didn't allow SkillsSummary block first time we load the page

--- src/components/HomeBanner/index.tsx
+++ src/components/HomeBanner/index.tsx
@@ -16,15 +16,6 @@ const HomeBanner: React.FC = () => {
                     </Typography>
                 </Box>
             </div>
-            <div className="light x1"></div>
-            <div className="light x2"></div>
-            <div className="light x3"></div>
-            <div className="light x4"></div>
-            <div className="light x5"></div>
-            <div className="light x6"></div>
-            <div className="light x7"></div>
-            <div className="light x8"></div>
-            <div className="light x9"></div>
         </div>
     );
 };
