6d3f2fd02c475010e3f491aebcf33321bbd4ed25
David Villalba Flores <david.selo@gmail.com>
2023-03-03 12:17:42 +0000

    Adding redux Adapter

--- src/features/posts/postsSlice.tsx
+++ src/features/posts/postsSlice.tsx
@@ -1,13 +1,29 @@
-import {createSlice, nanoid, createAsyncThunk} from '@reduxjs/toolkit';
+import {
+    createSlice,
+    createEntityAdapter,
+    nanoid,
+    createAsyncThunk,
+    createSelector,
+} from '@reduxjs/toolkit';
 // import {sub} from 'date-fns';
 import {RootState} from 'app/store';
 import {client} from 'api/client.js';
 
-const initialState = {
-    posts: [],
+// const initialState = {
+//     posts: [],
+//     status: 'idle',
+//     error: null,
+// };
+
+// Creating Adapter
+const postsAdapter = createEntityAdapter({
+    sortComparer: (a, b) => b.date.localeCompare(a.date),
+});
+
+const initialState = postsAdapter.getInitialState({
     status: 'idle',
     error: null,
-};
+});
 
 export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
     const response = await client.get('/fakeApi/posts');
@@ -54,7 +70,8 @@ const postsSlice = createSlice({
         },
         postUpdated(state, action) {
             const {id, title, content} = action.payload;
-            const existingPost = state.posts.find(post => post.id === id);
+            // const existingPost = state.posts.find(post => post.id === id);
+            const existingPost = state.entities[id];
             if (existingPost) {
                 existingPost.title = title;
                 existingPost.content = content;
@@ -62,7 +79,8 @@ const postsSlice = createSlice({
         },
         reactionAdded(state, action) {
             const {postId, reaction} = action.payload;
-            const existingPost = state.posts.find(post => post.id === postId);
+            const existingPost = state.entities[postId];
+            // const existingPost = state.posts.find(post => post.id === postId);
             if (existingPost) {
                 existingPost.reactions[reaction]++;
             }
@@ -76,14 +94,15 @@ const postsSlice = createSlice({
             .addCase(fetchPosts.fulfilled, (state, action) => {
                 state.status = 'succeeded';
                 // Add any fetched posts to the array
-                state.posts = state.posts.concat(action.payload);
+                // state.posts = state.posts.concat(action.payload);
+                // Use the `upsertMany` reducer as a mutating update utility
+                postsAdapter.upsertMany(state, action.payload);
             })
+            // Use the `addOne` reducer for the fulfilled case
+            .addCase(addNewPost.fulfilled, postsAdapter.addOne)
             .addCase(fetchPosts.rejected, (state, action) => {
                 state.status = 'failed';
                 state.error = action.error.message;
-            })
-            .addCase(addNewPost.fulfilled, (state, action) => {
-                state.posts.push(action.payload);
             });
     },
 });
@@ -91,12 +110,26 @@ const postsSlice = createSlice({
 export const {postAdded, postUpdated, reactionAdded} = postsSlice.actions;
 
 // Selectors
-export const selectAllPosts = (state: RootState) => {
-    console.log('Select All posts', state.posts);
-    return state.posts.posts;
-};
+// export const selectAllPosts = (state: RootState) => {
+//     console.log('Select All posts', state.posts);
+//     return state.posts.posts;
+// };
+
+// Export the customized selectors for this adapter using `getSelectors`
 
 export const selectPostById = (state: RootState, postId: string) =>
     state.posts.posts.find(post => post.id === postId);
+
+export const {
+    selectAll: selectAllPosts,
+    selectById: selectPostById,
+    selectIds: selectPostIds,
+    // Pass in a selector that returns the posts slice of state
+} = postsAdapter.getSelectors(state => state.posts);
+
+export const selectPostsByUser = createSelector(
+    [selectAllPosts, (state, userId) => userId],
+    (posts, userId) => posts.filter(post => post.user === userId),
+);
 
 export default postsSlice.reducer;
