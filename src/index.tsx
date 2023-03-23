03e9eed7416f2a2f4d699058441e2d9e256ddecf
David Villalba Flores <david.selo@gmail.com>
2023-03-23 12:04:48 +0000

    - Adding redirection from #! paths to friendly path - Adding history package

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 import {ViewportProvider} from 'providers/Viewport';
+import {createBrowserHistory} from 'history';
+
+const history = createBrowserHistory();
 
 const client = new ApolloClient({
     uri: 'http://localhost:3000/api/graphql',
@@ -22,15 +25,27 @@ let theme = createTheme({
 });
 theme = responsiveFontSizes(theme);
 
-ReactDOM.render(
-    <React.StrictMode>
-        <ApolloProvider client={client}>
-            <ViewportProvider>
-                <ThemeProvider theme={theme}>
-                    <App />
-                </ThemeProvider>
-            </ViewportProvider>
-        </ApolloProvider>
-    </React.StrictMode>,
-    document.getElementById('root'),
-);
+const app = document.getElementById('root');
+
+if (app) {
+    // 1. Set up the browser history with the updated location
+    // (minus the # sign)
+    const path = (/#!(\/.*)$/.exec(location.hash) || [])[1];
+    if (path) {
+        history.replace(path);
+    }
+
+    // 2. Render our app
+    ReactDOM.render(
+        <React.StrictMode>
+            <ApolloProvider client={client}>
+                <ViewportProvider>
+                    <ThemeProvider theme={theme}>
+                        <App />
+                    </ThemeProvider>
+                </ViewportProvider>
+            </ApolloProvider>
+        </React.StrictMode>,
+        app,
+    );
+}
