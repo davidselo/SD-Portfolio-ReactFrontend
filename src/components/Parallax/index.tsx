beb2021cfbbd3c2a0d9e0d755c0eea867b10a1ba
David Villalba Flores <david.selo@gmail.com>
2023-03-27 16:22:07 +0100

    Adding Parallax on Desktop version

--- src/components/Parallax/index.tsx
+++ src/components/Parallax/index.tsx
@@ -3,6 +3,8 @@ import React, {useRef} from 'react';
 import {Parallax, ParallaxLayer, IParallax} from '@react-spring/parallax';
 import SectionItem from 'components/SectionItem';
 import ufoAlien from 'assets/images/ufo-2.svg';
+import starsBackgorund from 'assets/background/stars.svg';
+import cloud from 'assets/background/cloud.svg';
 
 import {
     useAdaptiveTriggers,
@@ -49,7 +51,7 @@ export const ParallaxAboutMe: React.FC = () => {
                     speed={0}
                     factor={3}
                     style={{
-                        backgroundImage: url('stars', true),
+                        backgroundImage: `url(${starsBackgorund})`,
                         backgroundSize: 'cover',
                     }}
                 />
@@ -85,6 +87,122 @@ export const ParallaxAboutMe: React.FC = () => {
                     >
                         <img src={ufoAlien} style={{width: '40%'}} />
                     </Box>
+                </ParallaxLayer>
+
+                {/* Clouds */}
+                <ParallaxLayer offset={1} speed={0.8} style={{opacity: 0.1}}>
+                    <img
+                        src={cloud}
+                        style={{
+                            display: 'block',
+                            width: '20%',
+                            marginLeft: '55%',
+                        }}
+                    />
+                    <img
+                        src={cloud}
+                        style={{
+                            display: 'block',
+                            width: '10%',
+                            marginLeft: '15%',
+                        }}
+                    />
+                </ParallaxLayer>
+
+                <ParallaxLayer offset={1.75} speed={0.5} style={{opacity: 0.1}}>
+                    <img
+                        src={cloud}
+                        style={{
+                            display: 'block',
+                            width: '20%',
+                            marginLeft: '70%',
+                        }}
+                    />
+                    <img
+                        src={cloud}
+                        style={{
+                            display: 'block',
+                            width: '20%',
+                            marginLeft: '40%',
+                        }}
+                    />
+                </ParallaxLayer>
+
+                <ParallaxLayer offset={1} speed={0.2} style={{opacity: 0.2}}>
+                    <img
+                        src={cloud}
+                        style={{
+                            display: 'block',
+                            width: '10%',
+                            marginLeft: '10%',
+                        }}
+                    />
+                    <img
+                        src={cloud}
+                        style={{
+                            display: 'block',
+                            width: '20%',
+                            marginLeft: '75%',
+                        }}
+                    />
+                </ParallaxLayer>
+
+                <ParallaxLayer offset={1.6} speed={-0.1} style={{opacity: 0.4}}>
+                    <img
+                        src={cloud}
+                        style={{
+                            display: 'block',
+                            width: '20%',
+                            marginLeft: '60%',
+                        }}
+                    />
+                    <img
+                        src={cloud}
+                        style={{
+                            display: 'block',
+                            width: '25%',
+                            marginLeft: '40%',
+                        }}
+                    />
+                    <img
+                        src={cloud}
+                        style={{
+                            display: 'block',
+                            width: '10%',
+                            marginLeft: '80%',
+                        }}
+                    />
+                </ParallaxLayer>
+
+                <ParallaxLayer offset={2.6} speed={0.4} style={{opacity: 0.6}}>
+                    <img
+                        src={cloud}
+                        style={{
+                            display: 'block',
+                            width: '20%',
+                            marginLeft: '5%',
+                        }}
+                    />
+                    <img
+                        src={cloud}
+                        style={{
+                            display: 'block',
+                            width: '15%',
+                            marginLeft: '75%',
+                        }}
+                    />
+                </ParallaxLayer>
+                <ParallaxLayer
+                    offset={2.7}
+                    speed={-0.4}
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        pointerEvents: 'none',
+                    }}
+                >
+                    <img src={url('earth')} style={{width: '60%'}} />
                 </ParallaxLayer>
             </Parallax>
         </Box>
